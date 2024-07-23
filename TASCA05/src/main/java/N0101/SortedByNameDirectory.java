59d544c5e1e11188a288460c46b2ea0b78d2b8c2
paumaneja <paumaneja@gmail.com>
2024-07-23 18:29:48 +0200

    sprint01 tasca05 nivell01 exercici1

--- TASCA05/src/main/java/N0101/SortedByNameDirectory.java
+++ TASCA05/src/main/java/N0101/SortedByNameDirectory.java
@@ -2,6 +2,7 @@ package N0101;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedByNameDirectory {
 
@@ -9,33 +10,31 @@ public class SortedByNameDirectory {
 
         String path;
 
-        if (args.length > 0){
-            path = args[0];
-            System.out.println("The path to list is: " + path);
-
-            File directory = new File(path);
-
-            if (directory.exists() && directory.isDirectory()){
-                String[] files = directory.list();
-                Arrays.sort(files);
-                for (String s : files){
-                    System.out.println(s);
-                }
-            } else{
-                System.out.println("The directory does not exist or is not valid.");
-            }
-        } else {
-            System.out.println("You must enter the path (args) ");
+        if (args.length < 1){
+            System.out.println("You must enter the path as an argument. ");
+            return;
         }
 
+        path = args[0];
+        System.out.println("The path to list is: " + path);
 
+        File directory = new File(path);
 
+        if (!directory.exists() || !directory.isDirectory()) {
+            System.out.println("The directory does not exist or is not valid.");
+            return;
+        }
 
-
-
-
-
-
+        File[] files = directory.listFiles();
+        if (files != null){
+            Arrays.sort(files, Comparator.comparing(File::getName));
+            System.out.println("Directory " + path + " sorted by name.");
+            for (File file : files){
+                System.out.println(file.getName());
+            }
+        } else {
+            System.out.println("Error");
+        }
 
     }
 }
